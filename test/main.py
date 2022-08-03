dddbb0f694c224fb80f3c2eab34e04e99c5bc2e9
Taiki <roiashbel0113@gmail.com>
2022-08-03 18:25:14 +0900

    イニシャルコミット

--- test/main.py
+++ test/main.py
@@ -1,8 +1,11 @@
-from bs4 import BeautifulSoup
-
-while True :
-    
-    str = input()
-    print(eval(str))
-
+input_line = input("入力")
+moji= len(input_line)
+one_line = "++"
+three_line = "++"
+for i in range(moji):
+    one_line += "+"
+    three_line += "+"
+print(one_line)
+print("+" + input_line + "+")
+print(three_line)
 
